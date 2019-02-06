1f31ee04b5e83f9056637fe5c895b46fd644d4f5
pallemjensen <31887698+pallemjensen@users.noreply.github.com>
2019-02-06 16:21:45 +0100

    prod environment corrected

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {AngularFireModule} from "@angular/fire";
 import {environment} from "../environments/environment";
-import {AngularFirestoreCollection, AngularFirestoreModule} from "@angular/fire/firestore";
+import {AngularFirestoreModule} from "@angular/fire/firestore";
 
 @NgModule({
   declarations: [
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyCKQQ19Zy3K3Lbsbw2ueqn3ILzJaJYJtcA',
+    authDomain: 'fir-angular-app-28153.firebaseapp.com',
+    databaseURL: 'https://fir-angular-app-28153.firebaseio.com',
+    projectId: 'fir-angular-app-28153',
+    storageBucket: 'fir-angular-app-28153.appspot.com',
+    messagingSenderId: '966994851840'
+  }
 };
+
