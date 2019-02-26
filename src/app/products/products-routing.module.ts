468ba5f977f0d84866302ee2f3a8de3ca51eae56
pallemjensen <31887698+pallemjensen@users.noreply.github.com>
2019-02-26 02:51:50 +0100

    refactored to and addeed add component and used child routing to get there

--- src/app/products/products-routing.module.ts
+++ src/app/products/products-routing.module.ts
@@ -1,8 +1,13 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
 import {ProductListComponent} from './product-list/product-list.component';
+import {ProductAddComponent} from "./product-add/product-add.component";
 
 const routes: Routes = [
+  {
+    path: 'add',
+    component: ProductAddComponent
+  },
   {
     path: '',
     component: ProductListComponent
