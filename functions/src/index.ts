b875cc059c55fe0ac51100986c5bc038984aea95
pallemjensen <31887698+pallemjensen@users.noreply.github.com>
2019-03-17 14:42:09 +0100

    function made to store metadata in firestore on create or change in storage

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -3,6 +3,29 @@ import * as admin from 'firebase-admin';
 
 admin.initializeApp()
 
+exports.uploadNewProductImage = functions.storage.object().onFinalize((object) => {
+
+  return new Promise( (resolve, reject) => {
+    if (object && object.name && object.metadata){
+      const fileMeta = {
+        lastModified:  object.updated,
+        name: object.metadata.originalName,
+        type: "image/png",
+        size: object.size
+      };
+      const nameForDoc = object.name.split('/')[1];
+      admin.firestore().collection('files')
+        .doc(nameForDoc)
+        .set(fileMeta)
+        .then(metaDataValue => resolve(metaDataValue))
+        .catch(err => reject (err))
+    }
+    else {
+      reject('Error happened, no metadata or filedata');
+    }
+  })
+});
+
 exports.products = functions.https.onRequest((request, response) => {
   admin.firestore().collection('products')
     .get().then(products => {
