62bfa6a01f8c0527f96bedfbb6d460455959dc0f
pallemjensen <31887698+pallemjensen@users.noreply.github.com>
2019-03-17 17:20:31 +0100

    delete working with async await

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -6,23 +6,24 @@ admin.initializeApp()
 exports.deleteProduct = functions.firestore
   .document('products/{productID}')
   .onDelete((snap, context) => {
-    return new Promise( (resolve, reject) => {
+    return new Promise( async (resolve, reject) => {
       const deletedProduct = snap.data();
       if (deletedProduct) {
-        admin.firestore().collection('files')
-          .doc(deletedProduct.pictureId)
-          .delete()
-          .then(() => {
-            admin.storage()
+          try{
+            await admin.firestore().collection('files')
+              .doc(deletedProduct.pictureId)
+              .delete()
+              .then();
+
+            const resultFromStorage = await admin.storage()
               .bucket().file('product-pictures/' + deletedProduct.pictureId)
               .delete()
-              .then(result => resolve(result), err => reject(err))
-              .catch(err => reject(err))
-          },
-              err => reject(err))
-          .catch(err => reject(err))
-      } else {
-        reject('No product was deleted!');
+              .then()
+
+            resolve(resultFromStorage);
+          } catch (err) {
+            reject(err)
+          }
       }
     });
   });
