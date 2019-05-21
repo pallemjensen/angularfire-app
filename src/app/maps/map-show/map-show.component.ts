e60a7af4623c1cccebee137697259f66076a9b5a
pallemjensen <31887698+pallemjensen@users.noreply.github.com>
2019-05-21 12:07:16 +0200

    map updated

--- src/app/maps/map-show/map-show.component.ts
+++ src/app/maps/map-show/map-show.component.ts
@@ -1,13 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import {FriendService } from "../../friends/shared/friend.service";
-import {Observable} from "rxjs";
-import {Friend} from "../../friends/shared/friend.model";
-import {map, tap} from "rxjs/operators";
-import * as firebase from 'firebase/app';
-import GeoPoint = firebase.firestore.GeoPoint;
-import {Marker} from "@agm/core/services/google-maps-types";
-import {AgmMarker} from "@agm/core";
-
 
 @Component({
   selector: 'app-map-show',
@@ -41,28 +33,8 @@ export class MapShowComponent implements OnInit {
       });
   }
 
-  title = 'Friendfinder';
-  EASVMarkerLat: number = 55.488432;
-  EASVMarkerLng: number = 8.4465819;
-
-
-  //    getFriendsLocation(event): Observable<Friend[]> {
-  //   return this.getFriendsLocation();
-  //   location ? : GeoPoint;
-  // }
-
   zoom: number = 2;
   locationChosen = false;
-
-
-
-    //= [
-  //   {
-  //     lat: this.EASVMarkerLat,
-  //     lng: this.EASVMarkerLng,
-  //     label: 'EASV',
-  //   }
-  // ];
 
   onChoseLocation(event) {
     this.lat = event.coords.lat;
@@ -74,14 +46,7 @@ export class MapShowComponent implements OnInit {
     console.log(`clicked the marker: ${label || index}`)
   }
 
-  // mapClicked(event) {
-  //   this.markers.push({
-  //     lat: event.coords.lat,
-  //     lng: event.coords.lng,
-  //   });
-  // }
 }
-
 
 interface marker {
   lat: number;
