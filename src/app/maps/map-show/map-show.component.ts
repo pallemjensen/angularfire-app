61d291272ea823d29ae6bc8b2ee6da0224625702
pallemjensen <31887698+pallemjensen@users.noreply.github.com>
2019-05-21 11:25:36 +0200

    showing friends on map

--- src/app/maps/map-show/map-show.component.ts
+++ src/app/maps/map-show/map-show.component.ts
@@ -8,38 +8,38 @@ import GeoPoint = firebase.firestore.GeoPoint;
 import {Marker} from "@agm/core/services/google-maps-types";
 import {AgmMarker} from "@agm/core";
 
+
 @Component({
   selector: 'app-map-show',
   templateUrl: './map-show.component.html',
   styleUrls: ['./map-show.component.css']
 })
 export class MapShowComponent implements OnInit {
-  friends: Observable<Friend>;
   lat: number;
   lng: number;
   name: string;
+  markers: marker[];
 
   constructor(private friendService: FriendService) {
+    this.markers = new Array();
   }
 
   ngOnInit() {
+    this.showFriendsOnMap();
   }
 
-  // showFriendsOnMap(): marker[]{
-  //   Friend = this.friendService.getFriends()
-  //     .pipe(
-  //       tap(friends => {
-  //         friends.forEach(friend => {
-  //           this.markers.push({
-  //             lat : friend.location.latitude,
-  //             lng: friend.location.longitude,
-  //             label: friend.name
-  //           })
-  //         });
-  //       })
-  //     );
-  //   return this.markers;
-  // }
+  showFriendsOnMap(){
+     this.friendService.getFriends()
+      .subscribe( friends => {
+        friends.forEach(friend => {
+          this.markers.push({
+            lat: friend.location.latitude,
+            lng: friend.location.longitude,
+            label: friend.name
+          })
+        })
+      });
+  }
 
   title = 'Friendfinder';
   EASVMarkerLat: number = 55.488432;
@@ -51,28 +51,18 @@ export class MapShowComponent implements OnInit {
   //   location ? : GeoPoint;
   // }
 
-  zoom: number = 8;
+  zoom: number = 2;
   locationChosen = false;
 
 
 
-  markers: marker[] = [
-    {
-      lat: this.EASVMarkerLat,
-      lng: this.EASVMarkerLng,
-      label: 'EASV',
-    },
-    {
-      lat: 51.373858,
-      lng: 7.215982,
-      label: 'B',
-    },
-    {
-      lat: 51.723858,
-      lng: 7.895982,
-      label: 'C',
-    }
-  ]
+    //= [
+  //   {
+  //     lat: this.EASVMarkerLat,
+  //     lng: this.EASVMarkerLng,
+  //     label: 'EASV',
+  //   }
+  // ];
 
   onChoseLocation(event) {
     this.lat = event.coords.lat;
