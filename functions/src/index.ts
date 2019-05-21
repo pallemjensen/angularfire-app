da540bbe1242de7fc1b4bd23003a91dea9f5e592
pallemjensen <31887698+pallemjensen@users.noreply.github.com>
2019-05-21 12:37:00 +0200

    Update index.ts

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,10 +1,7 @@
 import * as admin from 'firebase-admin';
 import * as deleteFriend from './delete-friend-funtion';
-// import * as productRest from './products-rest-endpoint';
-
 
 admin.initializeApp();
-
 
 module.exports = {
   ...deleteFriend,
