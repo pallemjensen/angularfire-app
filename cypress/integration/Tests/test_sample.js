2e6e6ff74e89afff62df619ec6ab59b2c430a53c
langehk <mort120c@easv365.dk>
2019-05-23 11:40:11 +0200

    added cypress tests

--- cypress/integration/Tests/test_sample.js
+++ cypress/integration/Tests/test_sample.js
@@ -1,23 +1,70 @@
-describe('Routing test', function () {
+describe('Route to home', function () {
   it('should route to home ', function () {
     cy.visit('http://localhost:4200/home')
     cy.contains('friends').click()
   });
 })
-describe('Add friends test',function () {
+describe('Add friends route test',function () {
   it('should route to add friends', function () {
     cy.visit('http://localhost:4200/friends/add')
     cy.contains('Add friend').click()
+  });
 })
 
-  describe('Contains file selector', function () {
-    it('should contain file selector', function () {
-      cy.visit('http://localhost:4200/friends/add')
-      cy.contains('file').click()
-    });
-
-  })
-});
+describe('Map route test', function() {
+  it('should route to map', function () {
+    cy.visit('http://localhost:4200/maps')
+  });
+})
 
 
 
+describe('Freinds title', function () {
+  it('should contain a h1 tag', function () {
+    cy.visit('http://localhost:4200/friends')
+    cy.contains("Friends:")
+  });
+})
+
+describe('clicking "map" shows the right headings', function () {
+  it('should show the correct headings', function () {
+  cy.visit('http://localhost:4200/maps')
+  cy.contains('Map')
+  cy.contains('Home')
+  cy.contains('Friends')
+  });
+})
+
+describe('Friend form creation', function () {
+  it('should have friend fields', function () {
+    cy.visit('http://localhost:4200/friends/add')
+    cy.get('form').contains('Add friend')
+  });
+
+})
+
+describe('Show frontpage picture', () => {
+  it('Displayed the image', function () {
+    cy.visit('http://localhost:4200/home')
+    cy.get('img').should('be.visible');
+  });
+})
+
+describe('Email test', function () {
+  it('Gets, types and asserts', function () {
+    cy.visit('http://localhost:4200/friends/add')
+    cy.url('include', '/friends/add')
+
+    cy.get('mail')
+      .type('friend@email.com')
+      .should('have.value', 'fake@email.com')
+  });
+})
+
+
+describe('Contains file selector', function () {
+  it('should contain file selector', function () {
+    cy.visit('http://localhost:4200/friends/add')
+    cy.contains('file').click()
+  });
+})
