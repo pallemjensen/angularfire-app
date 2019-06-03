0a9b3a856a5b547dfebbcc786d870271275a8340
Bomholt <svenbomholt@gmail.com>
2019-06-03 23:34:54 +0200

    Revert "cypress updated"
    This reverts commit dba56e181850716004a2bab3cec59b522a72f200.

--- src/app/files/shared/file.service.spec.ts
+++ src/app/files/shared/file.service.spec.ts
@@ -1,4 +1,5 @@
 import {getTestBed, TestBed} from '@angular/core/testing';
+
 import { FileService } from './file.service';
 import {HttpClientTestingModule, HttpTestingController} from '@angular/common/http/testing';
 import {AngularFirestore, AngularFirestoreModule} from '@angular/fire/firestore';
