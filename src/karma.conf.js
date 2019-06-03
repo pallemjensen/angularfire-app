8534deb4d89ef67725cb1a08d8076ea31dbc27cd
Bomholt <svenbomholt@gmail.com>
2019-06-03 23:34:09 +0200

    Revert "karma firefox browser"
    This reverts commit 602187de78ea6369c6ff2c1d5bb1ffa9889dc679.

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -8,7 +8,7 @@ module.exports = function (config) {
     plugins: [
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
-      require('karma-firefox-launcher'),
+      require('karma-edge-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
       require('@angular-devkit/build-angular/plugins/karma')
@@ -26,7 +26,7 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Firefox'],
+    browsers: ['Chrome', 'Edge'],
     singleRun: false
   });
 };
